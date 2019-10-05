06ee66161d340e6fb355767f605fba45d8138c86
davidtb <davidtejerobravo97@gmail.com>
2019-10-06 00:01:39 +0200

    Cuadrícula del tablero

--- app/src/main/java/com/example/tetrisg8/GameView.java
+++ app/src/main/java/com/example/tetrisg8/GameView.java
@@ -12,6 +12,7 @@ import android.widget.LinearLayout;
 
 public class GameView extends View {
     private Rect block;
+    private Pieza pieza;
 
     public GameView(Context context) {
         super(context);
@@ -22,7 +23,25 @@ public class GameView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        pintarCuadricula(canvas);
+        pieza = new Pieza(1);
+        pieza.dibujarPieza(canvas);
+    }
+
+    public void pintarCuadricula(Canvas canvas){
         Paint p = new Paint();
-        
+        p.setColor(Color.GRAY);
+        int anchoCelda = this.getWidth() / 10;
+        int ejeX = 0;
+        for (int i = 0; i < 9; i++){
+            ejeX += anchoCelda;
+            canvas.drawLine(ejeX, 0, ejeX, this.getHeight(), p);
+        }
+        int altoCelda = this.getHeight() / 20;
+        int ejeY = 0;
+        for (int i = 0; i < 19; i++){
+            ejeY += altoCelda;
+            canvas.drawLine(0, ejeY, this.getWidth(), ejeY, p);
+        }
     }
 }
