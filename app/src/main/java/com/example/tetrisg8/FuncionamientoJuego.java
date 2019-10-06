445f6e6965117890e931657a72fb544d1614c306
sbsam <48331351+sbsam@users.noreply.github.com>
2019-10-06 09:51:14 +0200

    No es necesaria
    Los metodos de actualizar puntuacion en Tablero y generar ficha en fichaview.

--- app/src/main/java/com/example/tetrisg8/FuncionamientoJuego.java
+++
@@ -1,9 +0,0 @@
-package com.example.tetrisg8;
-
-public class FuncionamientoJuego {
-
-    public int actualizarPuntuacion(int puntuacion, Tablero tablero, Pieza pieza){ //le pasamos la puntuación actual
-        puntuacion = puntuacion + lineasCompletas(pieza)*30;
-        return puntuacion;
-    }
-}
