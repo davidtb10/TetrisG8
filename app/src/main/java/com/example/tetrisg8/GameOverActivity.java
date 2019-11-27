3de1aa1a393c75482e23268ffcfaec824b4aa242
ArshiaSaleem98 <45390300+ArshiaSaleem98@users.noreply.github.com>
2019-11-27 23:07:35 +0100

    Update GameOverActivity.java

--- app/src/main/java/com/example/tetrisg8/GameOverActivity.java
+++ app/src/main/java/com/example/tetrisg8/GameOverActivity.java
@@ -5,18 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+
 
 public class GameOverActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textViewPuntuacion;
     TextView textViewTiempo;
     DatabaseClass myDb;
     Button btnviewAll;
+    ImageView fotoUsuario;
+    ImageView[] listimage;
+    TextView[] nameTextView;
+    AlertDialog.Builder dialogBuilder;
+    Bitmap foto;
+    int cont;
     
 
 
@@ -29,6 +41,9 @@ public class GameOverActivity extends AppCompatActivity implements View.OnClickL
         //Mostramos la puntuación
         int tiempo = getIntent().getExtras().getInt("tiempo");
         DateFormat df = new SimpleDateFormat("mm:ss");
+        foto= (Bitmap)getIntent().getParcelableExtra("foto");
+        fotoUsuario= (ImageView) findViewById(R.id.imageViewPlayer);
+        fotoUsuario.setImageBitmap(foto);
         //Mostramos la puntuación
         textViewPuntuacion = (TextView) findViewById(R.id.score);
         textViewPuntuacion.setText(String.valueOf(puntuacion));
@@ -44,38 +59,28 @@ public class GameOverActivity extends AppCompatActivity implements View.OnClickL
         }
         myDb = new DatabaseClass(this);
         btnviewAll = (Button) findViewById(R.id.button_viewAll);
+        this.dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView= inflater.inflate(R.layout.ranking_dialog, null);
+
+        dialogBuilder.setView(dialogView);
+        listimage= new ImageView[]{(ImageView)dialogView.findViewById(R.id.imageView22), (ImageView)dialogView.findViewById(R.id.imageView23),(ImageView)dialogView.findViewById(R.id.imageView27),(ImageView)dialogView.findViewById(R.id.imageView28),(ImageView)dialogView.findViewById(R.id.imageView29),(ImageView)dialogView.findViewById(R.id.imageView30),(ImageView)dialogView.findViewById(R.id.imageView31),(ImageView)dialogView.findViewById(R.id.imageView32),(ImageView)dialogView.findViewById(R.id.imageView33),(ImageView)dialogView.findViewById(R.id.imageView34)};
+        nameTextView= new TextView[]{(TextView) dialogView.findViewById(R.id.textView2),(TextView) dialogView.findViewById(R.id.textView13),(TextView) dialogView.findViewById(R.id.textView14),(TextView) dialogView.findViewById(R.id.textView15),(TextView) dialogView.findViewById(R.id.textView16),(TextView) dialogView.findViewById(R.id.textView17),(TextView) dialogView.findViewById(R.id.textView18),(TextView) dialogView.findViewById(R.id.textView19),(TextView) dialogView.findViewById(R.id.textView20),(TextView) dialogView.findViewById(R.id.textView21)};
+
         viewAll();
     }
 
-    private void viewAll() {
+     private void viewAll() {
         btnviewAll.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Cursor res = myDb.getAllData();
-                        if (res.getCount() == 0) {
-                            // show message
-                            showMessage("Top Scores", "Nothing found");
-                            return;
-                        }
-                       int cont=1;
-                        StringBuffer buffer = new StringBuffer();
-                        while (res.moveToNext()) {
-                            buffer.append(cont+".");
-                            buffer.append("  Name :"+res.getString(1) + "\n");
-                            buffer.append("     Score :"+ res.getString(2) + "\n");
-                            buffer.append("     Time :"+res.getString(3) + "\n");
-
-                            buffer.append("\n");
-                            cont++;
-                        }
-
-                        // MUESTRA NOMBRES CON PUNTUACIONES
-                        showMessage("                    TOP SCORES              ", buffer.toString());
+                        showMessage();
                     }
                 }
         );
     }
+
 
     @Override
     public void onBackPressed() {
@@ -83,13 +88,34 @@ public class GameOverActivity extends AppCompatActivity implements View.OnClickL
     }
 
     public void showMessage(String title, String Message) {
-        android.app.AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.AlertDialogCustom);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(Message);
-        AlertDialog dialog = builder.create();
-        dialog.show();
-        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dark);
+        Cursor res = myDb.getAllData();
+  cont=0;
+
+
+
+        if (res.getCount() == 0) {
+            // show message
+
+            Toast toast1 =Toast.makeText(getApplicationContext(),
+                    "NOTHING FOUND", Toast.LENGTH_SHORT);
+
+            toast1.show();
+            return;
+        }
+        while(res.moveToNext()){
+            if (cont < 10) {
+                nameTextView[cont].append("     Name :" +res.getString(1) + "\n");
+                nameTextView[cont].append("     Score:"+res.getString(2) + "\n");
+                nameTextView[cont].append("     Time :" +res.getString(3) );
+
+
+                Bitmap image = BitmapFactory.decodeByteArray(res.getBlob(4),0,res.getBlob(4).length);
+                listimage[cont].setImageBitmap(image);
+            }
+            cont++;
+        }
+
+        dialogBuilder.create().show();
     }
 
 
